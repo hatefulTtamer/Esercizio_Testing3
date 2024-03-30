4099965c4eea30685a42b14c3c890b554596c3f2
Ttamer <mericomattia@gmail.com>
2024-03-30 14:07:13 +0100

    corretto Esercizio_Testing3

--- src/MainTest.java
+++ src/MainTest.java
@@ -1,22 +1,29 @@
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
     @org.junit.jupiter.api.Test
-    void getDateTest() {
-        String result = Main.getDate();
+    void getDate_Test() {
+        OffsetDateTime input = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        String result = input.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG));
         assertEquals("1 marzo 2023", result);
     }
 
     @org.junit.jupiter.api.Test
-    void getDateTest_2() {
-        String result = Main.getDate();
-        assertEquals("01/03/2023", result);
+    void getDate_Test2() {
+        OffsetDateTime input = OffsetDateTime.parse("2020-08-15T13:00:00Z");
+        String result = input.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG));
+        assertEquals("15 agosto 2020", result);
     }
 
     @org.junit.jupiter.api.Test
-    void getDateTest_3() {
-        String result = Main.getDate();
-        assertEquals("1 mar 2023", result);
+    void getDate_Test3() {
+        OffsetDateTime input = OffsetDateTime.parse("1986-04-22T13:00:00Z");
+        String result = input.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG));
+        assertEquals("22 aprile 1986", result);
     }
 }
